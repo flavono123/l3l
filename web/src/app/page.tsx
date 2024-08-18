b45b3cff466694f2384718b238e1ccf746fdd493
flavono123 <flavono123@gmail.com>
2024-08-18 13:56:20 +0900

    feat(web): search filtering on ns select
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -160,6 +160,7 @@ export default function App() {
                             value: namespace,
                           })),
                         )}
+                        filteringType="auto"
                       />
                     ) : null
                   }
